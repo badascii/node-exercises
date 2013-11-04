2a8c15a20d8790355d21a14327a3e71c69294123
Jordan A. Skarbek <jaskarbek@gmail.com>
2013-11-04 09:36:39 -0800

    Create command line app to asynchronously accept user info

---
+++ async.js
@@ -0,0 +1,35 @@
+var read = require('read');
+
+var Contact = function(name, address, phone) {
+  this.name    = '';
+  this.address = '';
+  this.phone   = '';
+};
+
+Contact.prototype.toString = function() {
+  return "Name: " + this.name + " Phone: " + this.phone + " Address: " + this.address;
+};
+
+var contact = new Contact();
+
+read({ prompt: '> '}, getName);
+
+function getName(err, result, isDefault) {
+  console.log("Please enter your name: ");
+  read({prompt: '> '}, getPhone);
+}
+
+function getPhone(err, result, isDefault) {
+  contact.name = result;
+  console.log("Please enter your phone number: ");
+  read({prompt: '> '}, getAddress);
+}
+
+function getAddress(err, result, isdefault) {
+  contact.phone = result;
+  console.log("Please enter your address: ");
+  read({prompt: '> '}, function(err, result) {
+    contact.address = result;
+    console.log(contact.toString());
+  });
+}
